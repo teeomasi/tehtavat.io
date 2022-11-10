4b5c9e7a3cc498ead8a28d61c6a6cc1341e1533f
teeomasi <112539960+teeomasi@users.noreply.github.com>
2022-11-10 20:39:46 +0200

    slider

--- 08B/main.js
+++ 08B/main.js
@@ -7,3 +7,11 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 5000);
